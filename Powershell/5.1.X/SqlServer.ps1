44e8717485fee2f45d62f3e263ad550d2ef65e58
bearfly <bearfly1990@163.com>
2017-09-23 07:10:14 +0800

    Simple query for SqlServer

---
+++ Powershell/5.1.X/SqlServer.ps1
@@ -0,0 +1,15 @@
+$dataSource = "SQLServer Instance Name"
+$database = "Database Name"
+$connectionString = "Server=$dataSource;Database=$database;IntegratedSecurity=True;"
+
+$query = "Select column from table where column = 'value'"
+
+$connection = "New-Object System.Data.SqlClient.SqlConnection"
+$connection.ConnectionString = $connectionString
+$connection.Open()
+$command = $connection.CreateCommand()
+$command.CommandText = $query
+
+$result = command.ExecuteScalar()
+
+$connection.Close()
