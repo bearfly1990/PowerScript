19f767c1d11b95fd685044bf382eee60b435656e
bearfly <bearfly1990@163.com>
2017-11-05 22:38:18 +0800

    refactor home page view to use a template

--- Python3/superlists/lists/tests.py
+++ Python3/superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -13,7 +13,9 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))
 # Create your tests here.
