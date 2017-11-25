96ada24279283ac1915b3a9b9e996458bad89571
bearfly <bearfly1990@163.com>
2017-11-25 23:40:21 +0800

    New structure urls

--- Python3/superlists/lists/urls.py
+++ Python3/superlists/lists/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from lists import views
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/', include('lists.urls')),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^new$', views.new_list, name='new_list'),
+    
 ]
