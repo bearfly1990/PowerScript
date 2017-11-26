d60c16348250c0123ed47f131df0425e9bad47d9
bearfly1990 <bearfly1990@163.com>
2017-11-26 19:26:02 +0800

    Use Bootstrap to improve layout

--- Python3/superlists/lists/urls.py
+++ Python3/superlists/lists/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
     url(r'^new$', views.new_list, name='new_list'),
-    
 ]
