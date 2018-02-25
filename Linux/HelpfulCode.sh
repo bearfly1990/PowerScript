d45908ea775177f7fc9076a3da089910930999f7
bearfly <bearfly1990@163.com>
2018-02-25 23:44:29 +0800

    add script install mysql in Linux

--- Linux/HelpfulCode.sh
+++ Linux/HelpfulCode.sh
@@ -23,3 +23,22 @@ sudo add-apt-repository --remove ppa:notepadqq-team/notepadqq
 #安装Chrome
 sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sources.list.d/
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+#安装Mysql
+sudo apt-get install mysql-server
+
+sudo apt-get install mysql-client
+
+sudo apt-get install libmysqlclient-dev
+
+#安装过程中会提示设置密码什么的，注意设置了不要忘了，安装完成之后可以使用如下命令来检查是否安装成功：
+
+sudo netstat -tap | grep mysql
+
+#通过上述命令检查之后，如果看到有mysql 的socket处于 listen 状态则表示安装成功。
+
+#登陆mysql数据库可以通过如下命令：
+
+mysql -u root -p
+
+#-u 表示选择登陆的用户名， -p 表示登陆的用户密码，上面命令输入之后会提示输入密码，此时输入密码就可以登录到mysql。
