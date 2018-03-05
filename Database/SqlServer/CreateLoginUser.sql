c7fb72dfeaf442bb110bb7985428adcce04dad56
Chen, Xiong <bearfly1990@163.com>
2018-03-05 14:13:34 +0800

    Update CreateLoginUser.sql

--- Database/SqlServer/CreateLoginUser.sql
+++ Database/SqlServer/CreateLoginUser.sql
@@ -3,7 +3,7 @@ GO
 
 Use DBName;
 GO
-IF NOTEXISTS(SELECT * FROM sys.database_principals WHERE name = N'Pub52_QA')
+IF NOT EXISTS(SELECT * FROM sys.database_principals WHERE name = N'Pub52_QA')
 BEGIN
     CREATE USER [DBName] FOR LOGIN [DBName]
     EXEC sp_addrolemember N'db_owner', N'DBName'
