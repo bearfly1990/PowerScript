20062e298345780446d12a7049e7563d9326b816
Chen, Xiong <bearfly1990@163.com>
2018-08-19 20:56:47 +0800

    Update startImport.bat

--- Private/Engineering/ImportMessages/startImport.bat
+++ Private/Engineering/ImportMessages/startImport.bat
@@ -1,12 +1,7 @@
 @echo off
 net use h: /delete /y
-:: net use l: /delete /y
-:: net use m: /delete /y
-
 net use h: "\\xxx"
-:: net use l: "\\xxx\PAMINV800"
-:: net use l: "\\xxx\PFIINV800"
-:: net use m: "\\xxx"
+
 IF EXIST "\\xxx\Python36" SET PATH=%PATH%;"xxx\Python36"
 pushd %~dp0
 python Import.py
