e3a3ec031328f911350b3352b0467dc55d7db625
Chen, Xiong <bearfly1990@163.com>
2018-08-19 21:00:10 +0800

    Create cmutils_excel.py

---
+++ Python3/mylib/cmutils_excel.py
@@ -0,0 +1,54 @@
+#/usr/bin/python3
+"""
+author: xiche
+create at: 08/19/2018
+description:
+    Common Excel utils for python script
+Change log:
+Date        Author      Version    Description
+08/19/2018   xiche      1.0        Init, add style_range
+
+"""
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
+from openpyxl import Workbook
+
+def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
+    """
+    Apply styles to a range of cells as if they were a single cell.
+
+    :param ws:  Excel worksheet instance
+    :param range: An excel range to style (e.g. A1:F20)
+    :param border: An openpyxl Border
+    :param fill: An openpyxl PatternFill or GradientFill
+    :param font: An openpyxl Font object
+    """
+    top = Border(top=border.top)
+    left = Border(left=border.left)
+    right = Border(right=border.right)
+    bottom = Border(bottom=border.bottom)
+
+    first_cell = ws[cell_range.split(":")[0]]
+    if alignment:
+        ws.merge_cells(cell_range)
+        first_cell.alignment = alignment
+
+    rows = ws[cell_range]
+    if font:
+        first_cell.font = font
+
+    for cell in rows[0]:
+        cell.border = cell.border + top
+    for cell in rows[-1]:
+        cell.border = cell.border + bottom
+
+    for row in rows:
+        l = row[0]
+        r = row[-1]
+        l.border = l.border + left
+        r.border = r.border + right
+        if fill:
+            for c in row:
+                c.fill = fill
+	
+
+        
