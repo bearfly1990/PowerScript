4b488acb0dbf1655589abe0d0f32ad69767a7f69
Chen, Xiong <bearfly1990@163.com>
2018-10-11 09:44:01 +0800

    update back to io

--- Python3/mylib/cmutils_io.py
+++ Python3/mylib/cmutils_io.py
@@ -1,148 +1,283 @@
 #/usr/bin/python3
-'''
+"""
 author: xiche
 create at: 06/16/2018
 description:
-    Utils for log
+    Utils for io operation
 Change log:
 Date         Author      Version    Description
-08/31/2018    xiche      1.0.1      Add change_logging_file
-09/12/2018    xiche      1.1.0      Add change_logging_file
-09/27/2018    xiche      1.1.1      Add VMTalk
-'''
-import sys, time, logging
-sys.path.insert(0, r"\\nj.pfs.net\departments\Development\Team Engineering\xiche\pythonlib")
+06/16/2018    xiche      1.0.1      add replace variables features for TxtUtils
+06/17/2018    xiche      1.0.2      add TxtUtils.read_first_line
+07/30/2018    xiche      1.0.3      add cls for class method
+08/10/2018    xiche      1.0.4      add class PathUtils
+08/18/2018    xiche      1.0.5      add TxtUtils.read_string_from_txt
+08/30/2018    xiche      1.1.0      change the method names style
+09/03/2018    xiche      1.1.1      add read_txt_rows
+09/07/2018    xiche      1.2.0      add XmlUtils
+09/27/2018    xiche      1.2.1      add OSUtils.copydir to overwrite existed files
+                                    add TxtUtils.write_str_append_to_file_first_line 
+"""
+import csv
+import os
+import xml.etree.ElementTree as ET
+import shutil
 
-from colorlog import ColoredFormatter
-from threading import Thread
-from datetime import datetime
-from cmutils.cmutils_io import TxtUtils
-class Logger:
-    # __LOG_FORMATTER_FILE =  ''
-    # __LOG_FILE_PATH = ''
-    # __LOG_FORMATTER_CONSOLE = ''
+class OSUtils:
+    def copydir(source, dest):
+        """Copy a directory structure overwriting existing files"""
+        for root, dirs, files in os.walk(source):
+            if not os.path.isdir(root):
+                os.makedirs(root)
+            for file in files:
+                rel_path = root.replace(source, '').lstrip(os.sep)
+                dest_path = os.path.join(dest, rel_path)
+                if not os.path.isdir(dest_path):
+                    os.makedirs(dest_path)
+                shutil.copyfile(os.path.join(root, file), os.path.join(dest_path, file))
+            
+class PathUtils:
+
+    @staticmethod
+    def check_make_dir_exist(filePath):
+        fileDir = PathUtils.get_dir_name_from_full_path(filePath)
+        if(fileDir):
+            if not os.path.exists(fileDir):
+                os.makedirs(fileDir)
+            
+    @staticmethod
+    def get_dir_name_from_full_path(filePath):
+        fileDir = None
+        index1 = filePath.rfind("\\")
+        index2 = filePath.rfind("/")
+        index = index1 if index1 > index2 else index2
+        if(index > -1):
+            fileDir = filePath[0:index]
+        return fileDir
+        
+    @staticmethod
+    def get_filename_from_full_path(fullPath):
+        file_name = os.path.basename(fullPath)
+        # lastIndex = fullPath.rfind('\\')
+        # if(lastIndex == -1):
+            # lastIndex = fullPath.rfind('/')
+            
+        # if(lastIndex == -1):
+            # lastIndex = 0
+            
+        # return fullPath[lastIndex:]
+        return file_name
+    @staticmethod
+    def add_flag_to_file_name(file_name, flag):
+        index = file_name.rfind(".")
+        if(index > -1):
+            file_name = file_name[0:index] + '.' + flag + file_name[index:]
+        return file_name
+class CSVUtils:
+
+    @staticmethod
+    def write_list_to_csv_file(filePath, rowsList, delimiterX=',',quotecharX='|', quotingX=csv.QUOTE_MINIMAL):
+        with open(filePath, 'w', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=delimiterX, quotechar=quotecharX, quoting=quotingX)
+            for rows in rowsList:
+                spamwriter.writerow(rows)
+                
+    @staticmethod
+    def read_csv_rows_list(filePath):
+        csvRowsList = []
+        with open(filePath, newline='') as csvfile:
+            csvReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in csvReader:
+                csvRowsList.append(row)
+        return csvRowsList
+        
+    @staticmethod
+    def read_csv_dict_list(file_path):
+        csv_dict_list = []
+        with open(file_path, newline='') as csvfile:
+            csv_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+            for row in csv_reader:
+                csv_dict_list.append(row)
+        return csv_dict_list
+
+class TxtUtils:
+    @classmethod
+    def write_list_to_file_with_newline(cls, filePath, rowsList, mode='w'):
+        PathUtils.check_make_dir_exist(filePath)
+        with open(filePath, mode) as f:
+            for row in rowsList:
+                f.write("%s\n" % row)
+                
+    @classmethod            
+    def write_list_to_txt_file(cls, filePath, rowsList, mode='w'):
+        PathUtils.check_make_dir_exist(filePath)
+        with open(filePath, mode, newline='') as f:
+            for row in rowsList:
+                f.write("%s" % row)
+                
+    @classmethod
+    def write_str_to_file(cls, file_path, str, mode='w'):
+        cls.write_list_to_txt_file(file_path, [str], mode)
+        
+    @classmethod
+    def write_str_append_to_file_first_line(cls, file_path, str, mode='w'):
+        lines = []
+        try:
+            lines = cls.read_txt_rows_list(file_path)
+        except (FileNotFoundError, PermissionError):
+            print('{} Not Exist or No Permission'.format(file_path))
+        lines.insert(0, str)
+        cls.write_list_to_file_with_newline(file_path, lines, mode)
+             
+    @classmethod
+    def remove_first_line(cls, filePath):
+        rowList = cls.read_txt_rows_list_with_newline(filePath)
+        rowList = list(rowList)
+        cls.write_list_to_txt_file(filePath, rowList[1:])
     
-    # __LOG_LEVEL         = logging.INFO
+    @classmethod
+    def read_first_line(cls, filePath):
+        rowList = []
+        with open(filePath, newline='') as f:
+            rowList = f.read().splitlines()
+            rowList = (x for x in rowList if x.strip())
+            rowList = list(rowList)
+        return rowList[0] if len(rowList) > 0 else ""
     
-    # __HANDLER_STREAM    = None
-    # __HANDLER_FILE      = None
+    @classmethod
+    def read_string_from_txt(cls, filePath):
+        with open(filePath, newline='') as f:
+            return f.read()
+              
+    @classmethod
+    def read_txt_rows_list(cls, filePath):
+        rowList = []
+        with open(filePath, newline='') as f:
+            rowList = f.read().splitlines()
+            rowList = (x for x in rowList if x.strip())
+        return list(rowList)
+        
+    @classmethod
+    def read_txt_rows_list_with_newline(cls, filePath):
+        rowList = []
+        with open(filePath, newline='') as f:
+            rowList = f.readlines()
+            rowList = (x for x in rowList if x.strip())
+        return list(rowList)
+        
+    @classmethod
+    def replace_variables(cls, file_template, file_destination, dict):
+        new_lines = []
+        for line in cls.read_txt_rows_list_with_newline(file_template):
+            for key,val in dict.items():
+                line = line.replace(key, val)
+            new_lines.append(line)
+        cls.write_list_to_txt_file(file_destination, new_lines)
+        
+    @classmethod
+    def read_txt_rows(cls, file_path):
+        return len(cls.read_txt_rows_list(file_path))
+        
+    @classmethod
+    def count_rows_contains_str(cls, file_path, str_contains):
+        count = 0
+        with open(file_path, newline='') as f:
+            line = f.readline()
+            while(line):
+                if str_contains.lower() in line.lower() :
+                    count += 1
+                line = f.readline()
+        return count
+        
+class ConfigUtils:
+    @staticmethod
+    def set(filePath, section, key, value):
+        lines = []
+        new_lines = []
+        sec_expected = False
+        key_expected = False
+        set_ready    = False
+        with open(filePath, 'r', newline='') as f:
+            lines = f.readlines()
+            
+        for index, line in enumerate(lines):
+            is_section = "[" in line.strip().lower()
+            if(is_section and section.lower() in line.strip().lower()):
+                sec_expected = True
+                new_lines.append(line)
+                continue
+            if(sec_expected and key.lower() in line.lower()):
+                key_expected = True
+                line = "{0}={1}\r\n".format(key, value)
+                new_lines.append(line)
+                continue
+            if(is_section and sec_expected and not key_expected):
+                new_lines.append("{0}={1}\r\n".format(key, value))
+                sec_expected = False
+                key_expected = True
+            new_lines.append(line)
+               
+        with open(filePath, 'w', newline='') as f:
+            f.writelines(new_lines)
+
+class XmlUtils:
+    @classmethod
+    def update_attrib_value_by_xpath(cls, xml_file_path, xpath_str, attrib_str, attrib_new_value):
+        xml_et  = ET.parse(xml_file_path)
+        root    = xml_et.getroot()
+
+        element = root.find(xpath_str)
+
+        element.attrib[attrib_str] = attrib_new_value
+        xml_et.write(xml_file_path)
     
-    
+    @classmethod
+    def get_texts_by_xpath(cls, xml_file_path, xpath_str, tag):
+        xml_et  = ET.parse(xml_file_path)
+        root    = xml_et.getroot()
 
-    def __init__(self, **options):
-        self.__LOGGER                   = logging.getLogger(__name__)
-        
-        self.__LOG_LEVEL                = self.get_log_level(options.get("level"))
-        self.__LOG_FORMATTER_FILE       = logging.Formatter(options.get("format_file"))
-        self.__LOG_FILE_PATH            = options.get("logfile_path")
-        self.__LOG_FORMATTER_CONSOLE    = self.init_colored_formatter(options.get("format_console"))
-        
-        logging.root.setLevel(self.__LOG_LEVEL)
-        
-        self.__HANDLER_STREAM = logging.StreamHandler()
-        self.__HANDLER_STREAM.setLevel(self.__LOG_LEVEL)
-        self.__HANDLER_STREAM.setFormatter(self.__LOG_FORMATTER_CONSOLE)
+        els   = root.findall(xpath_str)
+        texts = []
+        for el in els:
+            text = el.find(tag).text
+            texts.append(text)
+        return texts
+       
+    @classmethod
+    def get_attribs_values_by_xpath(cls, xml_file_path, xpath_str, attrib):
+        xml_et  = ET.parse(xml_file_path)
+        root    = xml_et.getroot()
 
-        self.__HANDLER_FILE = logging.FileHandler(self.__LOG_FILE_PATH)
-        self.__HANDLER_FILE.setLevel(self.__LOG_LEVEL)
-        self.__HANDLER_FILE.setFormatter(self.__LOG_FORMATTER_FILE)
-        
-        self.__LOGGER.setLevel(self.__LOG_LEVEL)
-        self.__LOGGER.addHandler(self.__HANDLER_STREAM)
-        self.__LOGGER.addHandler(self.__HANDLER_FILE)
+        els   = root.findall(xpath_str)
+        attrib_values = []
+        for el in els:
+            attrib_value = el.attrib[attrib]
+            attrib_values.append(attrib_value)
+        return attrib_values
+    # data_service_et     = ET.parse(DATA_SERVICE_CONFIG)
+    # pooling_agent_et    = ET.parse(POOLING_AGENT_CONFIG)
 
-    def change_logging_file(self, file_name):
-        self.__LOGGER.removeHandler(self.__HANDLER_FILE)
-        self.__HANDLER_FILE = logging.FileHandler(file_name)
-        self.__HANDLER_FILE.setLevel(self.__LOG_LEVEL)
-        self.__HANDLER_FILE.setFormatter(self.__LOG_FORMATTER_FILE)
-        self.__LOGGER.addHandler(self.__HANDLER_FILE)
-        
-    @property
-    def logger(self):
-        return self.__LOGGER
+    # root_maps           = data_service_et.getroot()
+    # root_configurations = pooling_agent_et.getroot()
 
-    def get_log_level(self,x):
-        return {
-            'DEBUG':logging.DEBUG,
-            'INFO': logging.INFO,
-            'ERROR': logging.ERROR,
-            'WARN': logging.WARN
-        }.get(x, logging.INFO)
-        
-    def init_colored_formatter(self, format_str):
-        formatter = ColoredFormatter(
-            format_str,
-            datefmt=None,
-            reset=True,
-            log_colors={
-                'DEBUG':    'cyan',
-                'INFO':     'green',
-                'WARNING':  'bold_yellow',
-                'ERROR':    'bold_red',
-                'CRITICAL': 'bold_red,bg_white',
-            },
-            secondary_log_colors={},
-            style='%'
-        )
-        return formatter
-
-class ShowProcess():
-    pct_now = 0 # current progress
-    max_steps = 0 # total steps
-    max_arrow = 50 # length of the process bar
-    infoDone = 'done'
-
-    def __init__(self, max_steps, infoDone = 'Done'):
-        self.max_steps = max_steps
-        self.pct_now = 0
-        self.infoDone = infoDone
-        self.count_now = 0
-        self.total = 0
-
-    # [>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]100.00%
-    def show_process(self, pct_now=None):
-        if pct_now is not None:
-            self.pct_now = pct_now
-        else:
-            self.pct_now
-        # calc how many '>'
-        num_arrow = int(self.pct_now * self.max_arrow / self.max_steps) 
-        # calc how many '-'
-        num_line = self.max_arrow - num_arrow 
-        # calc percent of finished (xx.xx%)
-        percent = self.pct_now * 100.0 / self.max_steps
-        #'\r' means to the left
-        process_bar = "[{0}{1}] {2:.2f}% ({3}/{4})\r".format('>'*num_arrow, '-'*num_line, percent, self.count_now, self.total)
-        # print to console
-        sys.stdout.write(process_bar) 
-        sys.stdout.flush()
-        if self.pct_now >= self.max_steps:
-            self.close()
-
-    def close(self):
-        print('')
-        # print(self.infoDone)
-        self.pct_now = 0
+    # el_map = root_maps.find(".//Map")
+    # for i in range(client_num - 1):
+    #     temp_el_map         = copy.deepcopy(el_map)
+    #     temp_el_clientID    = temp_el_map.find(".//ClientID")
+    #     temp_el_clientID.text =  temp_el_clientID.text + str(i+1)
+    #     root_maps.append(temp_el_map)
 
 
-    def monitor_process(self):
-        while(True):   
-            self.show_process(self.pct_now)
-            time.sleep(1) 
-            if(self.pct_now >= 100):
-                self.show_process(100)
-                break
+    # el_client_specific_configuration = root_configurations.find(".//ClientSpecificConfiguration")
+    # el_client = root_configurations.find(".//ClientSpecificConfiguration/Client")
 
-    def startMonitor(self):
-        t = Thread(target=self.monitor_process, args=())
-        t.start()
-        
-class VMTalk():
-    def __init__(self, talk_path, who='INFO'):
-        self.talk_path = talk_path
-        self.who = who
-    def say(self, content):
-        current_time = datetime.now()
-        content = '{:%Y-%m-%d %H:%M:%S} [{}] - {}'.format(current_time, self.who, content)
-        TxtUtils.write_str_append_to_file_first_line(self.talk_path, content)
+    # for i in range( client_num - 1 ):
+    #     temp_el_client  = copy.deepcopy(el_client)
+    #     temp_clientID   = temp_el_client.get("ClientID")
+    #     temp_el_client.set("ClientID", temp_clientID + str(i+1))
+    #     el_client_specific_configuration.append(temp_el_client)
+    #     data_service_et.write(DATA_SERVICE_CONFIG_L_PATH)
+    #     pooling_agent_et.write(POOLING_AGENT_CONFIG_L_PATH)
+
+
+if __name__ == '__main__':
+    XmlUtils.update_attrib_value('PAMJobScheduler.exe.config',".//appSettings/add[@key='startBasetime']",'value','2222' )
